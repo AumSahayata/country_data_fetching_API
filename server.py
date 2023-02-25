7bb2be77b1e23d611cd56fe0f326f1b5df93e311
AumSahayata <94548994+AumSahayata@users.noreply.github.com>
2023-02-25 19:26:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from scrape import scrape
 
 app = Flask(__name__)
 
+@app.route("/")
+def defaultData(country='india'):
+    return scrape(country)
+
 @app.route("/<country>")
 def fetchdata(country='india'):
     return scrape(country)
